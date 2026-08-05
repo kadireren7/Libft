26bfad4e8a6ac86846ada74c4b346b15da384a32
Kadir Eren Altintas <kaaltint@k1m25s02.42istanbul.com.tr>
2026-08-05 12:34:18 +0300

    gereksiz dosya temizlendi

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,24 +6,24 @@
 /*   By: kaaltint@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 12:04:15 by kaaltint          #+#    #+#             */
-/*   Updated: 2026/08/05 12:04:16 by kaaltint         ###   ########.fr       */
+/*   Updated: 2026/08/05 12:33:18 by kaaltint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    const unsigned char *str;
-    size_t  i;
+	const unsigned char	*str;
+	size_t				i;
 
-    str = (const unsigned char *)s;
-    i = 0;
-    while (i < n)
-    {
-        if (str[i] == (unsigned char)c)
-            return ((void *)&str[i]);
-        i++;
-    }
-    return (NULL);
+	str = (const unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((void *)&str[i]);
+		i++;
+	}
+	return (NULL);
 }
