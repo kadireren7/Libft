210a8a2bc64a456d5561f31b82126c6ccf970c5c
Kadir Eren Altintas <kaaltint@k1m25s02.42istanbul.com.tr>
2026-08-05 19:26:40 +0300

    july 5 19.26

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,12 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kaaltint@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/05 14:36:20 by kaaltint          #+#    #+#             */
-/*   Updated: 2026/08/05 19:26:05 by kaaltint         ###   ########.fr       */
+/*   Created: 2026/08/05 19:15:34 by kaaltint          #+#    #+#             */
+/*   Updated: 2026/08/05 19:22:33 by kaaltint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+void ft_putendl_fd(char *s, int fd)
+{
+    write (fd, &s, 1);
+    write (fd, "\n", 1);
+}
+int main(void) 
+{
+    int fd;
+    fd = open("a",O_WRONLY);
+    ft_putendl_fd("a", fd);
+    close(fd);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kaaltint@student.42istanbul.com.tr         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 12:29:05 by kaaltint          #+#    #+#             */
-/*   Updated: 2026/08/05 14:39:10 by kaaltint         ###   ########.fr       */
+/*   Updated: 2026/08/05 19:24:17 by kaaltint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,6 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(const char *s);
 void	ft_putchar_fd(char c, int fd);
 void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
 
 #endif
